6a47f24f5febc6f22d35d349b7a44811455e6715
Randy <rloiseau0@gmail.com>
2025-04-17 16:50:39 +0200

    filter css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { ModalVideoComponent } from "./components/modal-video/modal-video.component";
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +14,20 @@ import { ModalVideoComponent } from "./components/modal-video/modal-video.compon
 })
 export class AppComponent {
   title = 'anime-tracker';
+
+  constructor(private router: Router, private renderer: Renderer2) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        if (event.url.includes('/anime')) {
+          this.renderer.setStyle(document.body, 'overflow-y', 'scroll');
+        } else {
+          this.renderer.removeStyle(document.body, 'overflow-y');
+        }
+      });
+  }
+
+
+
+
 }
